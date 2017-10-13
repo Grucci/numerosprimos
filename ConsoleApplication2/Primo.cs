1386da8b4b8b467618b34e189ab449615e618aea
Grucci <felipe.grucci@gmail.com>
2017-10-12 23:17:58 -0300

    subida de programa numeros primos

---
+++ ConsoleApplication2/Primo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Primo : IPrimo
+    {
+        public string nprimo(int numero)
+        {
+            int a=0;
+            for (int i = numero; i > 0; i--)
+            {
+                if (numero % i == 0)
+                {
+                    a++;
+                }
+            }
+
+            if (a == 2)
+            {
+                return "o número " + numero + " é Primo";
+            }
+            else
+            {
+                return "o número " + numero + " não é Primo";
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeroPrimo = new Primo();
+            Console.Write("Informe o número a ser verificado: ");
+           var resultado = numeroPrimo.nprimo(Convert.ToInt16(Console.ReadLine()));
+
+            Console.Write(resultado);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
